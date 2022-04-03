5f9d5a54fbf27bdbdddb788871fff63e752193b3
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-04-04 01:23:14 +0300

    prideta startinis skaicius, event listeneris kuris sureaguoja paspaudus ir funkcija, kuri suveikia paspaudimo metu

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
 
-let button = document.getElementById('btn__element');
-let counter = document.getElementById('btn__state');
+let button = document.getElementById('btn-element');
+let counter = document.getElementById('btn-state');
+let num = 0;
+// let number = 0;
+// let counterNumber = parseInt(counter, number);
+
+button.addEventListener('click', functionAdd);
+
+function functionAdd() {
+  num = num + 1;
+  console.log(num);
+  counter.textContent = num;
+}
